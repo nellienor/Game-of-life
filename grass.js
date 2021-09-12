4798df063753aa43e842c1c5ab04c1e85a833662
nellinor <nelli.ordyan.y@tumo.org>
2021-09-12 12:44:31 +0400

    edited the classes and added new class

---
+++ grass.js
@@ -0,0 +1,16 @@
+class Grass extends LivingCreature {
+ 
+    mul() {
+        this.life++;
+        let newCell = random(this.chooseCell(0));
+        if (newCell && this.life > 10) {
+            let x = newCell[0];
+            let y = newCell[1];
+            matrix[y][x] = 1;
+            let grass = new Grass(x, y);
+            grassArr.push(grass);
+            this.life = 0;
+        }
+    }
+}
+
